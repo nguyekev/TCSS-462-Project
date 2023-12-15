cc4ce6227fbd7ecd72a2cead1e0aac7687911c9a
vboxuser <vboxuser@linux.myguest.virtualbox.org>
2023-12-15 14:08:18 -0800

    test running callservice.sh and it works

--- java_template/test/callservice.sh
+++ java_template/test/callservice.sh
@@ -2,12 +2,15 @@
 # apply grayscale and put in bucket
 # JSON object to pass to Lambda Function
 bucketName=tcss.bucket.462.f23.image.bl
-# imageName=5xgkhmr1.png
 imageName=Bird.png
+img1Name=ImgGray.png
+img2Name=ImgGrayMirror.png
+img3Name=ImgGrayMirrorRotate.png
+
 # JSON object to pass to Lambda Function
 payload='{"bucketname":"tcss.bucket.462.f23.image.bl","filename":"Bird.png", "imagePath":"/images/Bird.png"}'
 
-echo "Invoking Lambda function using AWS CLI"
+echo "Invoking Lambda function using AWS CLI to upload image to S3"
 output=$(aws lambda invoke --invocation-type RequestResponse --function-name Image_uploaderimage --region us-east-2 --payload "$payload" /dev/null)
 
 echo ""
@@ -15,7 +18,7 @@ echo "JSON RESULT:"
 echo "$output" | jq
 echo ""
 
-jsonGray={"\"bucketname\"":"\"$bucketName\"","\"filename\"":"\"$imageName\"","\"outfilename\"":"\"ImgGray.png\""}
+jsonGray={"\"bucketname\"":"\"$bucketName\"","\"filename\"":"\"$imageName\"","\"outfilename\"":"\"$img1Name\""}
 
 echo "Invoking Lambda function using AWS CLI for Grayscale transformation"
 time output=`aws lambda invoke --invocation-type RequestResponse --function-name image_grayscale --region us-east-2 --payload $jsonGray /dev/stdout | head -n 1 | head -c -2 ; echo`
@@ -26,7 +29,7 @@ echo ""
 
 # apply mirror on top of grayscale and put in bucket
 # JSON object to pass to Lambda Function
-jsonMirror={"\"bucketname\"":"\"$bucketName\"","\"filename\"":"\"ImgGray.png\"","\"outfilename\"":"\"ImgGrayMirror.png\""}
+jsonMirror={"\"bucketname\"":"\"$bucketName\"","\"filename\"":"\"$img1Name\"","\"outfilename\"":"\"$img2Name\""}
 
 echo "Invoking Lambda function using AWS CLI for Mirror transformation"
 time output2=`aws lambda invoke --invocation-type RequestResponse --function-name image_mirrorimage --region us-east-2 --payload $jsonMirror /dev/stdout | head -n 1 | head -c -2 ; echo`
@@ -37,7 +40,7 @@ echo ""
 
 # apply rotation on top of mirror on top of grayscale and put in bucket
 # JSON object to pass to Lambda Function
-jsonRotate={"\"bucketname\"":"\"$bucketName\"","\"filename\"":"\"ImgGrayMirror.png\"","\"outfilename\"":"\"ImgGrayMirrorRotate.png\""}
+jsonRotate={"\"bucketname\"":"\"$bucketName\"","\"filename\"":"\"$img2Name\"","\"outfilename\"":"\"$img3Name\""}
 
 echo "Invoking Lambda function using AWS CLI for Rotation transformation"
 time output3=`aws lambda invoke --invocation-type RequestResponse --function-name image_rotateimage --region us-east-2 --payload $jsonRotate /dev/stdout | head -n 1 | head -c -2 ; echo`
@@ -45,3 +48,44 @@ echo ""
 echo "JSON RESULT:"
 echo $output3 | jq
 echo ""
+
+
+
+# working with rds sql
+
+#!/bin/bash
+
+# JSON object to pass to Lambda Function
+jsonurl1={"\"url\"":"\"https://s3.us-east-2.amazonaws.com/tcss.bucket.462.f23.image.bl/$img1Name\""}
+jsonurl2={"\"url\"":"\"https://s3.us-east-2.amazonaws.com/tcss.bucket.462.f23.image.bl/$img2Name\""}
+jsonurl3={"\"url\"":"\"https://s3.us-east-2.amazonaws.com/tcss.bucket.462.f23.image.bl/$img3Name\""}
+
+#echo "Invoking Lambda function using API Gateway"
+#time output=`curl -s -H "Content-Type: application/json" -X POST -d $json {API-GATEWAY-REST-URL}`
+#echo ""
+
+#echo ""
+#echo "JSON RESULT:"
+#echo $output | jq
+#echo ""
+
+echo "Invoking Lambda function using AWS CLI to load images url to MySQL"
+#time output=`aws lambda invoke --invocation-type RequestResponse --function-name {LAMBDA-FUNCTION-NAME} --region us-east-2 --payload $json /dev/stdout | head -n 1 | head -c -2 ; echo`
+time outputurl1=`aws lambda invoke --invocation-type RequestResponse --function-name ImageSql --region us-east-2 --payload $jsonurl1 /dev/stdout | head -n 1 | head -c -2 ; echo`
+
+echo ""
+echo "JSON RESULT:"
+echo $outputurl1 | jq
+echo ""
+
+# time outputurl2=`aws lambda invoke --invocation-type RequestResponse --function-name ImageSql --region us-east-2 --payload $json /dev/stdout | head -n 1 | head -c -2 ; echo`
+# echo ""
+# echo "JSON RESULT:"
+# echo $outputurl2 | jq
+# echo ""
+
+# time outputurl3=`aws lambda invoke --invocation-type RequestResponse --function-name ImageSql --region us-east-2 --payload $json /dev/stdout | head -n 1 | head -c -2 ; echo`
+# echo ""
+# echo "JSON RESULT:"
+# echo $outputurl3 | jq
+# echo ""
